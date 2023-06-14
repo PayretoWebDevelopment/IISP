308ee82a792fbe92ed6ebe0a9d764652ff9686cf
Mirai Yoshizaki <miguel.pajarillo@payreto.com>
2023-06-14 12:28:52 +0800

    updated superadmin employee list

--- resources/views/admin/superadmin-employee-list.blade.php
+++ resources/views/admin/superadmin-employee-list.blade.php
@@ -1,22 +1,35 @@
-<h2 class="font-bold mb-4">Superadmins</h2>
-        <table class="table-auto border-collapse w-full">
-            <thead>
-                <tr class="bg-gray-200">
-                    <th class="py-2 px-4 border">ID</th>
-                    <th class="py-2 px-4 border">Name</th>
-                    <th class="py-2 px-4 border">Role</th>
-                    <th class="py-2 px-4 border">Actions</th>
+<section class="mt-10">
+    <div class="bg-white border border-gray-200 rounded-lg shadow p-5">
+        <h2 class="font-semibold text-center mb-5">Superadmin List</h2>
+        <table class="min-w-full divide-y divide-gray-200" id="adminList" style="width:100%">
+            <thead class="bg-gray-50">
+                <tr>
+                    <th scope="col"
+                        class="px-12 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500">ID
+                    </th>
+                    <th scope="col"
+                        class="px-12 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500">
+                        Name
+                    </th>
+                    <th scope="col"
+                            class="px-12 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500">
+                            Role
+                        </th>
+                        <th scope="col"
+                            class="px-12 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500">
+                            Actions</th>
+                    </tr>
                 </tr>
             </thead>
-            <tbody>
+            <tbody class="bg-white divide-y divide-gray-200">
                 <!-- Iterate over superadmins and populate table rows -->
                 @foreach ($superadmins as $superadmin)
                     <tr>
-                        <td class="py-2 px-4 border">{{ $superadmin->id }}</td>
-                        <td class="py-2 px-4 border hover:bg-blue-700"><a
+                        <td class="px-4 py-4 text-sm font-medium text-gray-700 whitespace-nowrap">{{ $superadmin->id }}</td>
+                        <td class="px-4 py-4 text-sm font-medium text-gray-700 whitespace-nowrap"hover:bg-blue-700"><a
                                 href="/users/profile/{{ $superadmin->id }}">{{ $superadmin->name }}</a></td>
-                        <td class="py-2 px-4 border">{{ $superadmin->role }}</td>
-                        <td class="py-2 px-4 border">
+                        <td class="px-4 py-4 text-sm font-medium text-gray-700 whitespace-nowrap">{{ $superadmin->role }}</td>
+                        <td class="px-4 py-4 text-sm font-medium text-gray-700 whitespace-nowrap">
                             @if (auth()->user()->id == $superadmin->id)
                             <a href="/admin/employee-edit/{{ $superadmin->id }}"
                                 class="bg-green-500 hover:bg-green-700 text-white font-bold py-1 px-2 rounded inline-block">Edit</a>
@@ -26,3 +39,5 @@
                 @endforeach
             </tbody>
         </table>
+    </div>
+</section>
--- storage/framework/views/e9706a1f912c8ae1a9dc133dee4b6095.php
+++ storage/framework/views/e9706a1f912c8ae1a9dc133dee4b6095.php
@@ -1,22 +1,35 @@
-<h2 class="font-bold mb-4">Superadmins</h2>
-        <table class="table-auto border-collapse w-full">
-            <thead>
-                <tr class="bg-gray-200">
-                    <th class="py-2 px-4 border">ID</th>
-                    <th class="py-2 px-4 border">Name</th>
-                    <th class="py-2 px-4 border">Role</th>
-                    <th class="py-2 px-4 border">Actions</th>
+<section class="mt-10">
+    <div class="bg-white border border-gray-200 rounded-lg shadow p-5">
+        <h2 class="font-semibold text-center mb-5">Superadmin List</h2>
+        <table class="min-w-full divide-y divide-gray-200" id="adminList" style="width:100%">
+            <thead class="bg-gray-50">
+                <tr>
+                    <th scope="col"
+                        class="px-12 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500">ID
+                    </th>
+                    <th scope="col"
+                        class="px-12 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500">
+                        Name
+                    </th>
+                    <th scope="col"
+                            class="px-12 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500">
+                            Role
+                        </th>
+                        <th scope="col"
+                            class="px-12 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500">
+                            Actions</th>
+                    </tr>
                 </tr>
             </thead>
-            <tbody>
+            <tbody class="bg-white divide-y divide-gray-200">
                 <!-- Iterate over superadmins and populate table rows -->
                 <?php $__currentLoopData = $superadmins; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $superadmin): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
                     <tr>
-                        <td class="py-2 px-4 border"><?php echo e($superadmin->id); ?></td>
-                        <td class="py-2 px-4 border hover:bg-blue-700"><a
+                        <td class="px-4 py-4 text-sm font-medium text-gray-700 whitespace-nowrap"><?php echo e($superadmin->id); ?></td>
+                        <td class="px-4 py-4 text-sm font-medium text-gray-700 whitespace-nowrap"hover:bg-blue-700"><a
                                 href="/users/profile/<?php echo e($superadmin->id); ?>"><?php echo e($superadmin->name); ?></a></td>
-                        <td class="py-2 px-4 border"><?php echo e($superadmin->role); ?></td>
-                        <td class="py-2 px-4 border">
+                        <td class="px-4 py-4 text-sm font-medium text-gray-700 whitespace-nowrap"><?php echo e($superadmin->role); ?></td>
+                        <td class="px-4 py-4 text-sm font-medium text-gray-700 whitespace-nowrap">
                             <?php if(auth()->user()->id == $superadmin->id): ?>
                             <a href="/admin/employee-edit/<?php echo e($superadmin->id); ?>"
                                 class="bg-green-500 hover:bg-green-700 text-white font-bold py-1 px-2 rounded inline-block">Edit</a>
@@ -25,4 +38,6 @@
                     </tr>
                 <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
             </tbody>
-        </table><?php /**PATH C:\Users\user\Downloads\IISP\resources\views/admin/superadmin-employee-list.blade.php ENDPATH**/ ?>
+        </table>
+    </div>
+</section><?php /**PATH C:\Users\user\Downloads\IISP\resources\views/admin/superadmin-employee-list.blade.php ENDPATH**/ ?>
